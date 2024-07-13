9d7fe690104bda3a1ac05e7dad80a0797a78a9b5
石恒祥太 <166115005+xiangtai520@users.noreply.github.com>
2024-07-13 18:35:46 +0800

    1

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -20,17 +20,17 @@ const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
+    name: '石恒祥太',
     avatar: '',
-    username: 'nekomeowww',
+    username: 'xiangtai520',
     title: 'CoFan Space 原始创作者',
     desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+      { type: 'github', icon: 'github', link: 'https://github.com/xiangtai520' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/xiangtai_520' },
     ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
+    nameAliases: ['xiangtai520', '祥太', '石恒祥太', 'shota', 'xiangtai'],
+    emailAliases: ['cofan365@qq.com'],
   },
   {
     name: '絢香音',
