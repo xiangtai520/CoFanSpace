40f1f2a86d51a74d7c59b9ebf93b028133d068a8
石恒祥太 <166115005+xiangtai520@users.noreply.github.com>
2024-07-15 18:27:18 +0800

    1

--- metadata/index.ts
+++ metadata/index.ts
@@ -8,7 +8,8 @@ export const include = ['笔记', '生活']
 
 /** Repo */
 export const githubRepoLink = 'https://github.com/xiangtai520'
-
+/** Discord */
+export const x = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'cofan.xyz'
