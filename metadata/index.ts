c3c1ee91cf8c550724753abf4f69021da734119d
石恒祥太 <166115005+xiangtai520@users.noreply.github.com>
2024-09-30 17:47:17 +0800

    1

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
 export const siteName = 'CoFan Space'
 export const siteShortName = 'CoFan Space'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteDescription = '探索无限可能'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
